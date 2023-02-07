3b2e405bfe028a92db9d5583f692eaf07dc5aed9
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-06 21:25:45 -0300

    Inicio de projeto

---
+++ main.js
@@ -0,0 +1,7 @@
+function veja 'aqui(){
+    document.getElementById("Veja aqui ").innerHTML = "<p>Dois Hamburgures</p>";
+}
+function carImagem(){
+    document.getElementById("imagem").innerHTML = "<img
+    width='250'>"";
+}

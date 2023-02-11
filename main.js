fe2a126cc338ef944f0c0717aaeffd48a119cfbe
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-10 22:49:07 -0300

    Novas imagem

--- main.js
+++
@@ -1,7 +0,0 @@
-function veja 'aqui(){
-    document.getElementById("Veja aqui ").innerHTML = "<p>Dois Hamburgures</p>";
-}
-function carImagem(){
-    document.getElementById("imagem").innerHTML = "<img
-    width='250'>"";
-}

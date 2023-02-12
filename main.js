31485435981b5151c9561fc9ebe51bf2f2de7b98
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-11 22:52:35 -0300

    Implementação dos Botões

---
+++ main.js
@@ -0,0 +1,45 @@
+function button(){
+    if(document.getElementById("imagem1").classList.contains("Show")){
+      document.getElementById("imagem1").classList.remove("Show")
+      document.getElementById("imagem1").classList.add("noShow")
+      document.getElementById("button1").textContent  = "Esconder"
+  }else{
+    document.getElementById("imagem1").classList.remove("noShow")
+    document.getElementById("imagem1").classList.add("Show")
+    document.getElementById("button1").textContent  = "Mostrar"
+  }
+  }
+  function button(){
+    if(document.getElementById("imagem2").classList.contains("Show")){
+      document.getElementById("imagem2").classList.remove("Show")
+      document.getElementById("imagem2").classList.add("noShow")
+      document.getElementById("button2").textContent  = "Esconder"
+  }else{
+    document.getElementById("imagem2").classList.remove("noShow")
+    document.getElementById("imagem2").classList.add("Show")
+    document.getElementById("button2").textContent  = "Mostrar"
+  }
+  }
+  function button(){
+    if(document.getElementById("imagem3").classList.contains("Show")){
+      document.getElementById("imagem3").classList.remove("Show")
+      document.getElementById("imagem3").classList.add("noShow")
+      document.getElementById("button3").textContent  = "Esconder"
+  }else{
+    document.getElementById("imagem3").classList.remove("noShow")
+    document.getElementById("imagem3").classList.add("Show")
+    document.getElementById("button3").textContent  = "Mostrar"
+  }
+  }
+
+  function button(){
+    if(document.getElementById("imagem4").classList.contains("Show")){
+      document.getElementById("imagem4").classList.remove("Show")
+      document.getElementById("imagem4").classList.add("noShow")
+      document.getElementById("button4").textContent  = "Esconder"
+  }else{
+    document.getElementById("imagem4").classList.remove("noShow")
+    document.getElementById("imagem4").classList.add("Show")
+    document.getElementById("button4").textContent  = "Mostrar"
+  }
+  }

e6422786c73a5e53619c26149e0ddb6048f4fdae
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-11 22:53:48 -0300

    Implementacão dos Botões

--- main.js
+++ main.js
@@ -1,45 +1,20 @@
-function button(){
-    if(document.getElementById("imagem1").classList.contains("Show")){
-      document.getElementById("imagem1").classList.remove("Show")
-      document.getElementById("imagem1").classList.add("noShow")
-      document.getElementById("button1").textContent  = "Esconder"
-  }else{
-    document.getElementById("imagem1").classList.remove("noShow")
-    document.getElementById("imagem1").classList.add("Show")
-    document.getElementById("button1").textContent  = "Mostrar"
-  }
-  }
-  function button(){
-    if(document.getElementById("imagem2").classList.contains("Show")){
-      document.getElementById("imagem2").classList.remove("Show")
-      document.getElementById("imagem2").classList.add("noShow")
-      document.getElementById("button2").textContent  = "Esconder"
-  }else{
-    document.getElementById("imagem2").classList.remove("noShow")
-    document.getElementById("imagem2").classList.add("Show")
-    document.getElementById("button2").textContent  = "Mostrar"
-  }
-  }
-  function button(){
-    if(document.getElementById("imagem3").classList.contains("Show")){
-      document.getElementById("imagem3").classList.remove("Show")
-      document.getElementById("imagem3").classList.add("noShow")
-      document.getElementById("button3").textContent  = "Esconder"
-  }else{
-    document.getElementById("imagem3").classList.remove("noShow")
-    document.getElementById("imagem3").classList.add("Show")
-    document.getElementById("button3").textContent  = "Mostrar"
-  }
-  }
+function car1(){
+  document.getElementById("imagem1").style.display ="block"
 
-  function button(){
-    if(document.getElementById("imagem4").classList.contains("Show")){
-      document.getElementById("imagem4").classList.remove("Show")
-      document.getElementById("imagem4").classList.add("noShow")
-      document.getElementById("button4").textContent  = "Esconder"
-  }else{
-    document.getElementById("imagem4").classList.remove("noShow")
-    document.getElementById("imagem4").classList.add("Show")
-    document.getElementById("button4").textContent  = "Mostrar"
-  }
-  }
+}
+function car2(){
+  document.getElementById("imagem2").style.display ="block"
+
+}
+function car3(){
+  document.getElementById("imagem3").style.display ="block"
+
+}
+function car4(){
+  document.getElementById("imagem4").style.display ="block"
+}
+  
+    
+  
+  
+ 
